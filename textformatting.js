9101be686a4449b3d259611f534705876a455cdd
NesieNessy <vanessa.sa.zwing@gmail.com>
2022-07-02 08:49:01 +0200

    format menu first draft

--- textformatting.js
+++ textformatting.js
@@ -1,3 +1,11 @@
-var textForDrinkMenu = 'Heißgetränke\nEspresso\nEspresso,&nbsp;dopio\nCappuccino\nMandel,&nbsp;Hafer,&nbsp;Soja\nLatte&nbsp;Macchiato\nCafé Crema\nHeiße&nbsp;Schokolade\n' +
-'Chai Latte\nVanille,&nbsp;Karamell,&nbsp;2&nbsp;Hausmischungen\nChai&nbsp;Classic\nMate&nbsp;Classic\nIngwertee\nHonig,&nbsp;Agavendicksaft,&nbsp;ungesüßt\nDiverse&nbsp;Tees\nKräuterbeet, Affentanz, Wachmacher, Schlafmütze';
+var textForDrinkMenu = '<b>Heißgetränke</b><br>Espresso<br>Espresso, dopio<br>Cappuccino<br>Mandel, Hafer, Soja<br>Latte Macchiato<br>Café Crema<br>Heiße Schokolade<br>' +
+'Chai Latte (Vanille, Karamell, 2 Hausmischungen)<br>Chai Classic<br>Mate Classic<br>Ingwertee (Honig, Agavendicksaft, ungesüßt)<br>Diverse Tees (Kräuterbeet, Affentanz, Wachmacher, Schlafmütze)' +
+'<br><b>Drinks</b><br>Kaffeemartini<br>Liquid Cocaine<br>White Russian<br>Zimtnuss<br><b>Erfrischungen</b><br>Limonade (Hausgemacht: Limette, Minze, Thymian, Rosmarin)<br>' +
+'Eistee (Basic, Minze, Anis, Beeren)<br>Saftschorlen (Saftschorlen nach Angebot, Holunder-Zitrone)<br>"Vitalisierer" (Ingwer-Zimt, goldene Milch, Matcha-Kiwi, Guarana-Mandel)';
 document.getElementById('text-for-drink-menu').innerHTML = textForDrinkMenu;
+
+var textForFoodMenu = '<b>Essen</b><br>Energy Ball (Brownie, Cookie-Dough, Matcha, Süß-Salzig)<br>Crêpes süß' +
+'(Schoko-Haselnuss Mus, Erdnussmus, Beeren, Spekulatius, Dattelcreme, Puderzucker, Apfelmus, (Toppings: Obst, Nüsse)<br>' +
+'Crêpes herzhaft (Tomate-Rosmarin, Kürbis-Humus-Linsen, Rote Beete-Spinat-Walnuss, Karotte-Zucchini-Sonnenbl.Kerne (Toppings: Salat, Käse (vegan), Rucola))<br>' +
+'Müslivariationen (Morgens, nach Bedarf)<br>Smoothies (Mittags-abends, nach Bedarf)';
+document.getElementById('text-for-food-menu').innerHTML = textForFoodMenu;
